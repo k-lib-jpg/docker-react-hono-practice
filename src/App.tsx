3ff9ecccfd371a96e5b8f3f14866195431bca18c
k-lib-jpg <k-liberty080822@outlook.com>
2026-06-17 22:52:15 +0900

    second commit add useState and handleAddTodo function

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,9 @@
+import { useState } from "react";
 import "./App.css"
 
 const App = () => {
-  const todos = [
+  const [title, setTitle] = useState("");
+  const [todos, setTodos] = useState([
     { 
       id: 1, 
       title: "todo1",
@@ -17,13 +19,33 @@ const App = () => {
       title: "todo3", 
       completed: false 
     },
-  ]
+  ]);
+
+  const handleAddTodo = () => {
+    setTodos([
+      ...todos, 
+      {
+        id: todos.length + 1,
+        title: title,
+        completed: false
+      }
+    ])
+  }
 
   return (
     <>
     <div>
-      {todos.map((todo) => 
-      <div key={todo.id}>{todo.title}</div>)}
+      {title}
+      {todos.map((todo) => (
+      <div key={todo.id}>{todo.title}</div>
+      ))}
+    <input 
+      type="text" 
+      name="title" 
+      placeholder="Add a todo" 
+      onChange={(e)=>(setTitle(e.target.value))}
+    />
+    <button onClick={handleAddTodo}>Add</button>
     </div>
     </>
   )
